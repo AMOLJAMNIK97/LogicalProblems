c9c7fc0178a67cc4100a338d5726d8cb749fb344
Amoljamnik97 <ajamnik.mech@gmail.com>
2023-01-08 21:31:59 +0530

    [amol]- Solving LogicalProblems

---
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("choose the Option");
+            Console.WriteLine("1.Fibonacci Series");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch (Option)
+            {
+                case 1:
+                    FibonacciSeries fibonacci = new FibonacciSeries();
+                    fibonacci.CheckFibonacciSeries();
+                    break;
+                
+            }
+        }
+
+    }
+}
